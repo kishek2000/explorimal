086a99435d0a07a1caf6d952d5536bc04f27ce3f
Adi Kishore <adikishore@Adis-iMac.local>
2020-07-04 00:28:22 +1000

    fixed some errors

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -24,7 +24,7 @@ export function HomeScreen({ navigation }) {
         <Button
           title="Schedules"
           onPress={() => navigation.navigate("Schedule")}
-          color="#333333"
+          color="#7c0003"
         />
       </View>
 
--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Text, View } from "react-native";
-import { styles } from "../syles/Styles";
+import { styles } from "../styles/Styles";
 
 export function LoginScreen({ navigation }) {
   return (
